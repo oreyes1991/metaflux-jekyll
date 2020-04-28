8da01189235bdbedb4e8b3cfe0a3be62455c0dbb
Osmar Reyes <osmarreyesst@gmail.com>
2020-04-28 17:52:36 -0500

    other test4

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -11,21 +11,6 @@ function setActiveHeaderTab() {
 	changeHeaderActiveTab(tabName);
 }
 /**
- * test the url if has a header item 
- * @param {String} url pathName url
- * @returns {String} the tabname
- */
-function getTabName(url) {
-	if (url === '/') return url
-	const tabs = ['docs', 'community', 'blog'];
-	let resp = 'blog';
-	tabs.forEach((el) => {
-		const regEx = new RegExp(el, 'g');
-		resp = regEx.test(url) ? el : resp;
-	});
-	return resp;
-}
-/**
  * toggle active class to the header elementes
  * @param {String} name tab name
  */
